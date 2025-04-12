7a21f9c5be8e1e9d1f1de153d8ee551475193ba6
glowysourworm <aniv.sm.esports@gmail.com>
2025-04-12 11:48:06 -0500

    debugging deployment

--- passenger-startup.js
+++
@@ -1,5 +0,0 @@
-// entry.cjs
-function loadApp() {
-  const { app } = import("./dist/sm-esports-web/server/server.mjs"); // this is your normal entry file - (index.js, main.js, app.mjs etc.)
-}
-loadApp();
