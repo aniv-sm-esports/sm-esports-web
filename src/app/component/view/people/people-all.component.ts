1856adbf5f5cad9575e74e60fcb0b287e4a09c33
glowysourworm <aniv.sm.esports@gmail.com>
2025-04-22 13:49:39 -0500

    Added virtual scrolling back to the people-all page
    This will take some tweaking; but Angular has a way to add virtual scrolling sharing what components we already have.

--- src/app/component/view/people/people-all.component.ts
+++ src/app/component/view/people/people-all.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {UserService} from '../../../service/user.service';
 import {PersonRoleType, User} from '../../../model/repository/entity/user.model';
-import {NgForOf, SlicePipe} from '@angular/common';
+import {NgClass, NgForOf, NgStyle, SlicePipe} from '@angular/common';
 import {AppService} from '../../../service/app.service';
 import {Router} from '@angular/router';
 import {AvatarComponent, AvatarSize} from '../../control/avatar.component';
@@ -12,6 +12,9 @@ import {faCircle} from '@fortawesome/free-solid-svg-icons';
 import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 import {UserSearchPipe} from '../../../pipe/user-search.pipe';
 import {SlicePipeTyped} from '../../../pipe/slice-typed.pipe';
+import {PeopleDataSource} from '../../datasource/people.datasource';
+import {CdkFixedSizeVirtualScroll, CdkVirtualForOf} from '@angular/cdk/scrolling';
+import {CdkVirtualScrollViewport} from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'people-all',
@@ -21,40 +24,38 @@ import {SlicePipeTyped} from '../../../pipe/slice-typed.pipe';
     FaIconComponent,
     UserSearchPipe,
     SlicePipe,
-    SlicePipeTyped
+    SlicePipeTyped,
+    CdkVirtualForOf,
+    CdkVirtualScrollViewport,
+    CdkFixedSizeVirtualScroll,
+    NgStyle,
+    NgClass
   ],
   templateUrl: '../../template/view/people/people-all.component.html'
 })
 export class PeopleAllComponent {
 
-  protected readonly appService: AppService;
-  protected userService: UserService;
-  private readonly router: Router;
+  protected readonly peopleDataSource:PeopleDataSource;
 
   protected readonly faCircle = faCircle;
-
   protected readonly AvatarSize = AvatarSize;
-  protected pageSize:number = 30;
 
-  // "Injected" Data
-  //
-  protected people:User[] = [];
   protected peopleBoardSearch:SearchModel<User>;
   protected peopleGeneralSearch:SearchModel<User>;
 
-  constructor(appService:AppService, router: Router, userService:UserService) {
-    this.appService = appService;
-    this.userService = userService;
-    this.router = router;
+  constructor(protected readonly appService:AppService,
+              protected readonly userService:UserService,
+              protected readonly router: Router) {
+
     this.peopleBoardSearch = new SearchModel<User>({ "personRole": PersonRoleType.BoardMember } as User, ["personRole"]);
     this.peopleGeneralSearch = new SearchModel<User>({ "personRole": PersonRoleType.GeneralUser } as User, ["personRole"]);
+    this.peopleDataSource = new PeopleDataSource(this.userService);
 
-    // Reset the user search
-    //userService.resetSearch();
+    //this.userService.resetSearch();
 
     // Set a watch on the user name
     userService.onFilterChange().subscribe(() => {
-      this.reload();
+      //this.reload();
     });
 
     // Filtering is set up as a public member of UserService
@@ -64,10 +65,11 @@ export class PeopleAllComponent {
   reload() {
     this.userService.getAll()
         .then(users => {
-          this.people = users;
         });
   }
   loadPage(page:number) {
 
   }
+
+  protected readonly PersonRoleType = PersonRoleType;
 }
